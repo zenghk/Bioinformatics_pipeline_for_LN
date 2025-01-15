6f0f014684a9f2044782891fa38908443161d19d
zenghk <47877427+zenghk@users.noreply.github.com>
2025-01-15 15:38:26 +0800

    Update split_to_subfiles.py
    Remove personal information

--- Analysis/split_to_subfiles.py
+++ Analysis/split_to_subfiles.py
@@ -1,8 +1,3 @@
-#!/zzh_gpfs/apps/python/bin/python
-"""
-usage:
-	python *.py /path/to/data/dir/
-"""
 import os,sys,csv
 import subprocess
 from multiprocessing import Pool, Process, Manager
@@ -12,17 +7,6 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 def batch_iterator(iterator, batch_size) :
-	"""Returns lists of length batch_size.
-
-	This can be used on any iterator, for example to batch up
-	SeqRecord objects from Bio.SeqIO.parse(...), or to batch
-	Alignment objects from Bio.AlignIO.parse(...), or simply
-	lines from a file handle.
-
-	This is a generator function, and it returns lists of the
-	entries from the supplied iterator.  Each list will have
-	batch_size entries, although the final list may be shorter.
-	"""
 	entry = True
 	while entry :
 		batch = []
